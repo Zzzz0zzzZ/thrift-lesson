76b348ebbbc52d3baff1d67dda317d960eee3bc5
王 思哲 <1175690167@qq.com>
2022-07-15 16:08:32 +0800

    match server: 5.0

--- match_system/src/main.cpp
+++ match_system/src/main.cpp
@@ -63,34 +63,56 @@ class Pool{
 				cout << "ERROR: " << tx.what() << endl;
 			}
 		}
+        
+        bool check_match(uint32_t i, uint32_t j){
+            auto a = users[i], b=users[j];
+            int dt = abs(a.score - b.score);
+            int a_max_dif = wt[i] * 50;
+            int b_max_dif = wt[j] * 50;
+
+            return dt <= a_max_dif && dt <= b_max_dif;
+
+        }
+
 
 		void match(){
+
+            for(uint32_t i=0; i<wt.size(); i++){
+                wt[i]++;
+            }
+
 			while(users.size() > 1){
-				sort(users.begin(), users.end(), [&](User& a, User b){
-						return a.score < b.score;
-						});
 				bool flag = true;
-				for (uint32_t i=1; i<users.size(); i++){
-					auto a = users[i-1], b = users[i];
-					if(b.score - a.score <= 50){
-						users.erase(users.begin()+i-1, users.begin()+i+1);
-						save_result(a.id, b.id);
-						flag = false;
-						break;
-					}
+				for (uint32_t i=0; i<users.size(); i++){
+				    for(uint32_t j=i+1; j<users.size(); j++){
+
+                        if(check_match(i,j)){
+                            auto a = users[i], b = users[j];
+                            users.erase(users.begin()+j);
+                            users.erase(users.begin()+i);
+                            wt.erase(wt.begin()+j);
+                            wt.erase(wt.begin()+i);
+                            save_result(a.id, b.id);
+                            flag = false;
+                            break;
+                        }
+                    }
+
+                }
+                if(flag)    break;
 				}
-				if(flag)   break;
 			}
-		}
 
 		void add(User user){
 			users.push_back(user);
+            wt.push_back(0);
 		}
 
 		void remove(User user){
 			for(uint32_t i=0; i<users.size(); i++){
 				if(users[i].id == user.id){
 					users.erase(users.begin() + i);
+                    wt.erase(wt.begin() + i);
 					break;
 				}
 			}
@@ -98,6 +120,7 @@ class Pool{
 
 	private:
 		vector<User> users;
+        vector<int> wt;  // waitng time / :s
 }pool;
 
 class MatchHandler : virtual public MatchIf {
@@ -152,7 +175,6 @@ void consume_task(){
 			if(task.type == "add") pool.add(task.user);
 			else if(task.type == "remove") pool.remove(task.user);
 
-			pool.match();
 		}
 	}
 }
